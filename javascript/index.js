50d0bdc9a5c8d28b5a44ddbaf001851034f8cf33
oshimaharuna <ooooooshim115@gmail.com>
2018-10-03 23:42:41 +0900

    ログイン画面の実装

--- javascript/index.js
+++ javascript/index.js
@@ -16,3 +16,48 @@ $(function() {
     return false;
   });
 });
+
+var config = {
+  apiKey: "AIzaSyBznuHQZgE-Y9twkbOTLFWa2azJouNmf9s",
+  authDomain: "spaceshuttle-hp.firebaseapp.com",
+  databaseURL: "https://spaceshuttle-hp.firebaseio.com",
+  projectId: "spaceshuttle-hp",
+  storageBucket: "spaceshuttle-hp.appspot.com",
+  messagingSenderId: "772619856753"
+};
+firebase.initializeApp(config);
+
+//DOM取得
+var member = document.getElementById('member');
+var loginbutton = document.getElementById('loginbutton');
+var logout = document.getElementById('logout');
+
+//ログアウト処理
+logout.addEventListener('click', function() {
+  firebase.auth().signOut();
+});
+
+
+
+//認証状態の確認
+firebase.auth().onAuthStateChanged(function(user) {
+  if(user) {
+    loginDisplay();
+  }
+  else {
+    logoutDisplay();
+  }
+});
+
+function loginDisplay() {
+  logout.classList.remove('hide');
+	member.classList.remove('hide');
+  loginbutton.classList.add('hide');
+}
+
+
+function logoutDisplay() {
+  logout.classList.add('hide');
+	member.classList.add('hide');
+  loginbutton.classList.remove('hide');
+}
---
+++ javascript/login.js
@@ -0,0 +1,86 @@
+//firebaseの初期化
+var config = {
+  apiKey: "AIzaSyBznuHQZgE-Y9twkbOTLFWa2azJouNmf9s",
+  authDomain: "spaceshuttle-hp.firebaseapp.com",
+  databaseURL: "https://spaceshuttle-hp.firebaseio.com",
+  projectId: "spaceshuttle-hp",
+  storageBucket: "spaceshuttle-hp.appspot.com",
+  messagingSenderId: "772619856753"
+};
+firebase.initializeApp(config);
+
+//DOM取得
+var inputarea = document.getElementById('input-area');
+//var newuser = document.getElementById('newuser');
+var login = document.getElementById('login');
+var loginbutton = document.getElementById('loginbutton');
+var logout = document.getElementById('logout');
+var info = document.getElementById('info');
+var member = document.getElementById('member');
+
+
+
+//新規登録処理
+/*
+newuser.addEventListener('click', function(e) {
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+  .catch(function(error) {
+    alert('登録できません（' + error.message + '）');
+  });
+});
+*/
+
+
+//ログイン処理
+login.addEventListener('click', function(e) {
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+
+  firebase.auth().signInWithEmailAndPassword(email, password)
+  .catch(function(error) {
+    alert('ログインできません（' + error.message + '）');
+  });
+});
+
+
+
+//ログアウト処理
+logout.addEventListener('click', function() {
+  firebase.auth().signOut();
+});
+
+
+
+//認証状態の確認
+firebase.auth().onAuthStateChanged(function(user) {
+  if(user) {
+    loginDisplay();
+  }
+  else {
+    logoutDisplay();
+  }
+});
+
+
+
+function loginDisplay() {
+  logout.classList.remove('hide');
+  member.classList.remove('hide');
+  inputarea.classList.add('hide');
+  loginbutton.classList.add('hide');
+
+  info.textContent = "ログイン中です！";
+}
+
+
+function logoutDisplay() {
+  logout.classList.add('hide');
+  member.classList.add('hide');
+  inputarea.classList.remove('hide');
+  loginbutton.classList.remove('hide');
+
+  info.textContent = "";
+}
